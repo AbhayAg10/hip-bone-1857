b26574e6f27c7f4f36cd2a9a5cba783747ccaa99
AbhayAg10 <abhaytalkin@gmail.com>
2022-12-14 12:31:17 +0530

    setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
