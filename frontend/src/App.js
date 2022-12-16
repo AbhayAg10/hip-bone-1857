fa72a1f5f7c66a9aae4fef75b3289336641ca62a
AbhayAg10 <abhaytalkin@gmail.com>
2022-12-16 21:44:52 +0530

    day-4-abhay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import Navbar from "./Components/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
-        <Navbar />
-        <AllRoutes />
+        {/* <Navbar /> */}
+        {/* <AllRoutes /> */}
+        <Footer />
       </div>
     </Provider>
   );
--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
-import { Container, Divider, Flex, Stack } from '@chakra-ui/react'
+import { Box, Center, Container, Divider, Flex, Image, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
+import mainlogo from '../../Components/req/jobpandacom-logo.png'
 
 const Footer = () => {
   return (
@@ -16,16 +17,257 @@ const Footer = () => {
           h={"160px"}
           px={"148px"}
           maxW={"100%"}
-          border={"1px"}
-          borderColor={"black"}
+          // border={"1px"}
+          // borderColor={"black"}
         >
-          <Stack
+          <Flex
             direction={"row"}
           >
-
-          </Stack>
+            <Flex
+              direction={"column"}
+              pr={"17px"}
+              w={"180px"}
+              h={'100%'}
+              // border={"1px"}
+              // borderColor={"blue"} 
+            >
+              <Box
+                maxH={"70px"}
+                w={"100%"}
+                // border={"1px"}
+                // borderColor={"blue"} 
+              >
+                <Image cursor={'pointer'} maxH={"100%"} src={mainlogo} />
+              </Box>
+              <Text
+                mt={'20px'}
+                textAlign={'start'}
+                color={"#8292b4"}
+                fontSize={"16px"}
+                fontWeight={"400"}
+              >Connect with us</Text>
+              <Flex
+                h={"25px"}
+                w={'80%'}
+                // border={"1px"}
+                // borderColor={"blue"}
+                mb={"20px"}
+                mt={'5px'}
+              >
+                <Image cursor={'pointer'} mr={'16px'} src='https://static.naukimg.com/s/0/0/i/new-homepage/facebook.svg' />
+                <Image cursor={'pointer'} mr={'16px'} src='https://static.naukimg.com/s/0/0/i/new-homepage/instagram.svg' />
+                <Image cursor={'pointer'} mr={'16px'} src='https://static.naukimg.com/s/0/0/i/new-homepage/twitter.svg' />
+                <Image cursor={'pointer'} src='https://static.naukimg.com/s/0/0/i/new-homepage/linkedin.svg' />
+              </Flex>
+            </Flex>
+            <Flex
+              direction={"column"}
+              pl={"30px"}
+              w={"180px"}
+              h={'100%'}
+              // border={"1px"}
+              // borderColor={"blue"} 
+            >
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >About us</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Careers</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Employer home</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Sitemap</Text>
+            </Flex>
+            <Flex
+              direction={"column"}
+              pl={"30px"}
+              w={"180px"}
+              h={'100%'}
+              // border={"1px"}
+              // borderColor={"blue"} 
+            >
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Help center</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Summons/Notices</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Grievances</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Report issue</Text>
+            </Flex>
+            <Flex
+              direction={"column"}
+              pl={"30px"}
+              w={"180px"}
+              h={'100%'}
+              // border={"1px"}
+              // borderColor={"blue"} 
+            >
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Privacy policy</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Terms & conditions</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Fraud alert</Text>
+              <Text
+                textAlign={'start'}
+                pb={'12px'}
+                color={'#445578'}
+                fontSize={'16px'}
+                fontWeight={'400'}
+                lineHeight={'20px'}
+                cursor={'pointer'}
+                _hover={{
+                  color : "#457eff"
+                }}
+              >Trust & safety</Text>
+            </Flex>
+            <Flex
+              direction={'column'}
+              ml={'80px'}
+              h={"100%"}
+              w={'362px'}
+              p={"24px"}
+              borderRadius={'8px'}
+              border={"1px"}
+              borderColor={"gray.300"}
+            >
+              <Text
+                textAlign={'start'}
+                fontWeight={'700'}
+                lineHeight={"23px"}
+                fontSize={'16px'}
+              >Apply on the go</Text>
+              <Text
+                textAlign={'start'}
+                fontWeight={'400'}
+                lineHeight={"20px"}
+                fontSize={'13px'}
+                color={'#445578'}
+              >Get real-time job updates on our App</Text>
+              <Flex
+                pt={'16px'}
+                direction={'row'}
+                h={'65px'}
+                // border={"1px"}
+                // borderColor={"red"}
+              >
+                <Image cursor={'pointer'} src='https://static.naukimg.com/s/0/0/i/new-homepage/android-app.png' />
+                <Image cursor={'pointer'} ml={'10px'} src='https://static.naukimg.com/s/0/0/i/new-homepage/ios-app.png' />
+              </Flex>
+            </Flex>
+          </Flex>
         </Flex>
-        <Divider mx={"auto"} mt={"48px"} mb={"35px"} orientation={"horizontal"} w={"80%"} borderColor={"black"} />
+        <Divider mx={"auto"} mt={"48px"} mb={"35px"} orientation={"horizontal"} w={"100%"} borderColor={"gray.200"} />
         <Flex
           h={"45px"}
           px={"148px"}
@@ -33,6 +275,54 @@ const Footer = () => {
           border={"1px"}
           borderColor={"black"}
         >
+          <Flex
+            mr={'30px'}
+            w={'480px'}
+            h={'100%'}
+            // border={'1px'}
+            // borderColour={'red'}
+          >
+            <Image cursor={'pointer'} maxH={'60%'} src={'https://static.naukimg.com/s/0/0/i/new-homepage/infoedge-logo.svg'} />
+            <Box
+              h={'100%'}
+              ml={'32px'}
+            >
+              <Text 
+                fontWeight={'400'}
+                color={'#8292b4'} 
+                textAlign={'start'}
+                fontSize={'13px'}  
+              >All trademarks are the property of their respective owners</Text>
+              <Text
+                fontWeight={'400'}
+                color={'#8292b4'} 
+                textAlign={'start'}
+                fontSize={'13px'}
+              >All rights reserved © 2022 Info Edge (India) Ltd.</Text>
+            </Box>
+          </Flex>
+          <Box
+            w={'120px'}
+            h={'100%'}
+            p={'12px'}
+            // border={'1px'}
+            // borderColour={'green'}
+          >
+            <Text
+              fontWeight={'500'}
+              color={'gray.500'} 
+              textAlign={'start'}
+              fontSize={'13px'}
+            >Our businesses</Text>
+          </Box>
+          <Box
+            w={'534px'}
+            h={'100%'}
+            border={'1px'}
+            borderColour={'green'}
+          >
+
+          </Box>
         </Flex>
       </Container>
     </>
