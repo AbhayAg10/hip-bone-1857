57ff4ba8811763e8d9bad8df348e24ae2f254aaa
AbhayAg10 <abhaytalkin@gmail.com>
2022-12-16 12:55:09 +0530

    day-3 abhay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Footer />
     </div>
   );
 }
