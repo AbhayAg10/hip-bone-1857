98df5dd36c38534702fe91e6b6432f70ef575925
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-12-17 10:00:36 +0000

    Filter

--- backend/controllers/companies.controler.js
+++ backend/controllers/companies.controler.js
@@ -3,13 +3,21 @@ const asyncHandler = require("express-async-handler");
 const Company = require("../models/companies.model");
 
 const getCompanies = asyncHandler(async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const show = parseInt(req.query.show) || 10;
-  const companies = await Company.find({})
-    .limit(show)
-    .skip((page - 1) * show)
-    .exec();
-  res.status(200).json(companies);
+  // console.log(req.query)
+  // if(req.query){
+
+  //   // const companies = await Company.find().where();
+  //   res.status(200).json(companies);
+  // }else{
+
+    const page = parseInt(req.query.page) || 1;
+    const show = parseInt(req.query.show) || 10;
+    const companies = await Company.find({})
+      .limit(show)
+      .skip((page - 1) * show)
+      .exec();
+      res.status(200).json(companies);
+  // }
 });
 
 const getCompany = asyncHandler(async (req, res) => {
