7b7009eb999ff929d86bd6ada1c9d6a890069d38
Rohit Singh <kailash.jc.rs@gmail.com>
2022-12-18 17:48:42 +0530

    last day

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -4,6 +4,8 @@ import Companies from "../Components/Companies/Companies";
 import CompanyProfile from "../Components/Companies/CompanyProfile";
 import JobDetail from "../Components/Jobs/JobDetail";
 import JobPage from "../Components/Jobs/JobPage";
+import ServicesPage from "../Components/Services/ServicesPage";
+
 
 const AllRoutes = () => {
   return (
@@ -14,12 +16,9 @@ const AllRoutes = () => {
       <Route path='/companies/:id' element={<CompanyProfile />} />
       {/* Don't Touch it Ends*/}
 
-      <Route path='/jobpage' element={<JobPage />}>
-        JobPage
-      </Route>
-      <Route path='/jobpage/:jobId' element={<JobDetail />}>
-        JobPage
-      </Route>
+      <Route path='/jobpage' element={<JobPage />} />
+      <Route path='/jobpage/:jobId' element={<JobDetail />} />
+      <Route path='/services' element={<ServicesPage />} />
     </Routes>
   );
 };
